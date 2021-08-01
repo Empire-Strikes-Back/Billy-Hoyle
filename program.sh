1fad398076bf551b97a22ee86bdc5aa3529c7205
fiber <fiber@program>
2021-08-01 03:29:47 +0300

    i remove no-history push from program.sh

--- program.sh
+++ program.sh
@@ -11,16 +11,4 @@ main(){
 
 }
 
-push(){
-  ORIGIN=$(git remote get-url origin)
-  rm -rf .git
-  git init -b main
-  git remote add origin $ORIGIN
-  git config --local include.path ../.gitconfig
-  git add .
-  git commit -m "i am fiber program"
-  git push -f -u origin main
-}
-
-
 "$@"
