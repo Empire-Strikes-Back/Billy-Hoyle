98a6173a773866d52d1a20616c522aad10aeda95
fiber <fiber@program>
2021-08-01 19:27:11 +0300

    i add repl to program.sh

--- program.sh
+++ program.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
 repl(){
-  # lein repl :headless :host 0.0.0.0 :port 35543
-  lein repl :start :host 0.0.0.0 :port 35543
+  clj -M:repl
 }
 
 main(){
-
-  clojure -m fiber.main
-
+  clojure -M:main
 }
 
 "$@"
